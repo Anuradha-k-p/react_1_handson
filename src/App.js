839594a428f7244de3cd1bc0f43b9f5bb098aa50
Anuradha-k-p <72305218+Anuradha-k-p@users.noreply.github.com>
2023-09-11 00:09:22 +0530

    Update App.js
    bt

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 
+
 import './App.css';
-import Button_one from './Main/Result';
-import { Button_two } from './Main/Result';
+import Buttonone from './Main/Result';
+import { Buttontwo } from './Main/Result';
 
 function App() {
   return (
     <div className="App">
     <h1 className='title'>Styling using Functional Component</h1>
-     <Button_one/>
-     <Button_two/>
+     <Buttonone/>
+     <Buttontwo/>
     </div>
   );
 }
